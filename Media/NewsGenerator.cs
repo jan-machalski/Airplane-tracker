f19a3990d236d58fe0783a692a6b43c858299a94
Jan Machalski <janmachalski@outlook.com>
2024-04-03 23:00:19 +0200

    iterator added

--- Media/NewsGenerator.cs
+++ Media/NewsGenerator.cs
@@ -1,38 +1,85 @@
 ﻿using ExCSS;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
 
 namespace projekt_Jan_Machalski
 {
     public class NewsGenerator
     {
+        private MediaIterator iterator;
+        public NewsGenerator(List<IMedia> medias,List<IReportable> reportables)
+        {
+            iterator = new MediaIterator(medias,reportables);
+        }
+        public string GenerateNextNews()
+        {
+            
+            if(iterator.Current() == (null,null))
+            {
+                return null;
+            }
+
+
+            (IReportable reportable, IMedia media) = iterator.Current();
+            iterator.MoveNext();
+            return reportable.Report(media);
+   
+
+        }
+    }
+    public class MediaIterator:IEnumerator
+    {
         private List<IMedia> medias;
         private List<IReportable> reportables;
         private int mediaIndex;
         private int reportableIndex;
-        public NewsGenerator(List<IMedia> medias,List<IReportable> reportables)
+        public MediaIterator(List<IMedia> medias, List<IReportable> reportables)
         {
-            mediaIndex = -1;
+            mediaIndex = 0;
             reportableIndex = 0;
             this.medias = medias;
             this.reportables = reportables;
+            
         }
-        public string GenerateNextNews()
+        object IEnumerator.Current => Current();
+
+        public (IReportable?, IMedia?) Current()
         {
+            if((medias.Count == 0 && reportables.Count == 0) || reportableIndex>=reportables.Count)
+            {
+                return ((null,null));
+            }
+            return (reportables[reportableIndex], medias[mediaIndex]);
+        }
+        public bool MoveNext()
+        {
+            if (reportableIndex >= reportables.Count)
+                return false;
             mediaIndex++;
-            if(mediaIndex >= medias.Count)
+            if (mediaIndex >= medias.Count)
             {
                 reportableIndex++;
                 mediaIndex = 0;
             }
-            if (reportableIndex >= reportables.Count)
-                return null;
-            string news = reportables[reportableIndex].Report(medias[mediaIndex]);
-            return news;
+            return true;
         }
+        public (int,int) Key()
+        {
+            return (mediaIndex,reportableIndex);
+        }
+        public void Reset()
+        {
+            mediaIndex = 0;
+            reportableIndex = 0;
+        }
+
+
     }
     public class NewsListsCreator
     {
